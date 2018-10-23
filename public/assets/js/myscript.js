95ca17913eb399359521654c77329a8ca0178d06
Arman85 <advanced315@gmail.com>
2018-10-23 18:34:59 +0600

    change tab script

--- public/assets/js/myscript.js
+++ public/assets/js/myscript.js
@@ -2,7 +2,6 @@ function openTab( evt, tabName ){
 	
 	var i, tabcontent, tablinks;
 
-	// Get all elements with class="tabcontent" and hide them
 	// Получаю все элементы с классом tabcontent и прячу их
 	tabcontent = document.getElementsByClassName('tabcontent');
 	
@@ -10,7 +9,6 @@ function openTab( evt, tabName ){
 		tabcontent[i].style.display = "none";
 	}
 
-	// Get all elements with class="tablinks" and remove the class "active"
 	// Получаю все элементы с классом tablinks и удаляю класс active
 	tablinks = document.getElementsByClassName("tablinks");
 
@@ -18,7 +16,6 @@ function openTab( evt, tabName ){
 		tablinks[i].className = tablinks[i].className.replace("active", "");
 	}
 
-	// Show the current tab, and add an "active" class to the button that opened the tab
 	// Показываю текущую вкладку, и добавляю класс active кнопке которая открыла вкладку.
 	document.getElementById(tabName).style.display = "block";
 
